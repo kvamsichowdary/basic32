ce1ca61f7e422afcf8ab5b2b5ded91e6a1606c13
kvamsichowdary <vamsichowdary2002@gmail.com>
2022-11-16 10:51:52 +0530

    qwert

---
+++ satwik/src/main/java/com/kmit/in/satwik/App.java
@@ -0,0 +1,13 @@
+package com.kmit.in.satwik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
