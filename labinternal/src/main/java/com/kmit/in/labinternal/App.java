9fb47e989af4d6ffde5914423d143fca4c905ff9
kvamsichowdary <vamsichowdary2002@gmail.com>
2022-11-17 11:12:31 +0530

    added in exam

---
+++ labinternal/src/main/java/com/kmit/in/labinternal/App.java
@@ -0,0 +1,13 @@
+package com.kmit.in.labinternal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
